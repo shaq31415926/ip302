d5b0279e959d7e08bc8108b4a59b6016f79ba011
shaq31415926 <sarah.haq@artsymail.com>
2022-10-18 22:40:43 +0200

    helpers file

---
+++ src/helpers.py
@@ -0,0 +1,74 @@
+import numpy as np
+import pandas as pd
+
+
+def identify_highly_correlated_features(df, correlation_threshold):
+    """
+    This definition can be used to identify highly correlated features
+    
+    @param df pandas DataFrame
+    @param correlation_threshold int 
+    
+    @return a DataFrame with highly correlated features 
+    """
+    
+    corr_matrix = df.corr().abs() # calculate the correlation matrix with 
+    high_corr_var = np.where(corr_matrix >= correlation_threshold) # identify variables that have correlations above defined threshold
+    high_corr_var = [(corr_matrix.index[x], corr_matrix.columns[y], round(corr_matrix.iloc[x, y], 2))
+                         for x, y in zip(*high_corr_var) if x != y and x < y] # identify pairs of highly correlated variables
+    
+    high_corr_var_df = pd.DataFrame(high_corr_var).rename(columns = {0: 'corr_feature',
+                                                                     1: 'drop_feature',
+                                                                     2: 'correlation_values'})
+
+    
+    if high_corr_var_df.empty:
+        high_corr_var_df
+    else:
+        high_corr_var_df = high_corr_var_df.sort_values(by = 'correlation_values', ascending = False)
+
+    return high_corr_var_df
+
+
+
+def identify_missing_data(df):
+    """
+    This function is used to identify missing data
+    
+    @param df pandas DataFrame
+    
+    @return a DataFrame with the percentage of missing data for every feature and the data types
+    """
+    
+    percent_missing = df.isnull().mean()
+    
+    missing_value_df = pd.DataFrame(percent_missing).reset_index() # convert to DataFrame
+    missing_value_df = missing_value_df.rename(columns = {"index" : "feature",
+                                                                0 : "percent_missing"}) # rename columns
+
+    missing_value_df = missing_value_df.sort_values(by = ['percent_missing'], ascending = False) # sort the values
+    
+    data_types_df = pd.DataFrame(df.dtypes).reset_index().rename(columns = {"index" : "feature",
+                                                                0 : "data_type"}) # rename columns
+    
+    missing_value_df = missing_value_df.merge(data_types_df, on = "feature") # join the dataframe with datatype
+    
+    missing_value_df.percent_missing = round(missing_value_df.percent_missing*100, 2) # format the percent_missing
+    
+    return missing_value_df
+
+
+
+def one_hot(df, categorical_cols):
+    """
+    @param df pandas DataFrame
+    @param cols a list of columns to encode 
+    @return a DataFrame with one-hot encoding
+    """
+    
+    for c in categorical_cols:
+        dummies = pd.get_dummies(df[c], prefix=c)
+        df = pd.concat([df, dummies], axis=1)
+        df.drop(c, axis = 1, inplace = True)
+    
+    return df
